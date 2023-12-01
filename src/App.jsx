ba68c8e75a71586c8bc856bcce3d9d60c4e5c1db
simo <wambuisimon712@gmail.com>
2023-12-01 14:56:58 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useContext } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import { DarkModeContext } from "./context/DarkmodeContext";
+import Home from "./pages/home/Home";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
   return (
     <div className={`theme-${darkMode ? "dark" : "light"}`}>
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+import "./home.scss";
+import React from "react";
+
+const Home = () => {
+  return <div className="home">Home</div>;
+};
+
+export default Home;
